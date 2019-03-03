4b8572c502578fd1b20f786d369c442c45433856
convivial-system-4430 <cdneal091@gmail.com>
2019-03-03 17:47:47 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,10 +1,10 @@
-#def my_collect(yeet)
-#  rvalue = []
-#  i = 0
-#  
-#  while i < yeet.length
-#  rvalue << (yield yeet[i])
-#  i++
-#end
-#end
+def my_collect(yeet)
+  rvalue = []
+  i = 0
+  
+  while i < yeet.length
+  rvalue << (yield yeet[i])
+  i++
+end
+end
 
